7d2f0790e91d8eb98c0b48b8c03c5681fb01c3de
mahatmasmile <mahatmasmile@qq.com>
2016-08-22 01:16:51 +0800

    create project

---
+++ src/test/RedisDefined.java
@@ -0,0 +1,11 @@
+package test;
+
+import org.quickjedis.core.CacheFactory;
+import org.quickjedis.impl.Redis;
+
+public class RedisDefined {
+	public static Redis TestRedis;
+	static {
+		TestRedis = CacheFactory.GetRedis("TestRedis");
+	}
+}
